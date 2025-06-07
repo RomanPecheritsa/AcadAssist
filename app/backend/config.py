65b47c64c7e2825befa7300e796b17b4a8ddb554
pecheritsa <pecheritsa.roman@gmail.com>
2025-06-07 16:21:22 +0300

    changed config remove prod dev status

--- app/backend/config.py
+++ app/backend/config.py
@@ -1,18 +1,7 @@
-from enum import Enum
-from functools import lru_cache
-
 from pydantic_settings import BaseSettings
 
 
-class Environment(str, Enum):
-    DEVELOPMENT = "dev"
-    TESTING = "test"
-    PRODUCTION = "prod"
-
-
 class Settings(BaseSettings):
-    ENVIRONMENT: Environment = Environment.DEVELOPMENT
-
     APP_HOST: str
     APP_PORT: int
     DEBUG: bool
@@ -31,16 +20,9 @@ class Settings(BaseSettings):
         )
 
     class Config:
+        env_file = ".env"  # ← только один .env
         env_file_encoding = "utf-8"
 
-    @classmethod
-    def load_config(cls, environment: Environment = Environment.DEVELOPMENT):
-        return cls(_env_file=f".env.{environment.value}")
 
-
-@lru_cache
 def get_settings() -> Settings:
-    from os import getenv
-
-    env = getenv("ENVIRONMENT", "dev")
-    return Settings.load_config(Environment(env))
+    return Settings()
