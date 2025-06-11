b10a2d8abd4ee95452cefb6305fe75219228dd50
pecheritsa <pecheritsa.roman@gmail.com>
2025-06-11 14:42:43 +0300

    add endpoints /semesters CRUD, repository, service, depenpds, validation number filed

--- app/routes/university/specialty_routes.py
+++ app/routes/university/specialty_routes.py
@@ -23,27 +23,27 @@ async def get_specialty(specialty_id: int, service: SpecialtyService = Depends(g
 @router.post("/specialties", status_code=status.HTTP_201_CREATED, response_model=SpecialtyRead)
 async def create_specialty(
     request: SpecialtyCreate,
-    service: SpecialtyService = Depends(get_specialty_service),  # <-- Явно указываем
+    service: SpecialtyService = Depends(get_specialty_service),
 ):
-    return await service.create_specialty(request.dict())
+    return await service.create_specialty(request.model_dump())
 
 
 @router.put("/specialties/{specialty_id}", status_code=status.HTTP_200_OK, response_model=SpecialtyRead)
 async def update_specialty(
     specialty_id: int,
     request: SpecialtyCreate,
-    service: SpecialtyService = Depends(get_specialty_service),  # <-- Явно указываем
+    service: SpecialtyService = Depends(get_specialty_service),
 ):
     specialty = await service.get_specialty_by_id(specialty_id)
     if not specialty:
         raise HTTPException(status_code=404, detail="Specialty not found")
-    return await service.update_specialty(specialty, request.dict())
+    return await service.update_specialty(specialty, request.model_dump())
 
 
 @router.delete("/specialties/{specialty_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_specialty(
     specialty_id: int,
-    service: SpecialtyService = Depends(get_specialty_service),  # <-- Явно указываем
+    service: SpecialtyService = Depends(get_specialty_service),
 ):
     specialty = await service.get_specialty_by_id(specialty_id)
     if not specialty:
