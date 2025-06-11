8f6c78cf7151acd32f9ff8731499c78b5ce6c083
pecheritsa <pecheritsa.roman@gmail.com>
2025-06-11 13:52:32 +0300

    restructure project, add specialty enpoints

--- app/services/university_service.py
+++ app/services/university_service.py
@@ -1,5 +1,5 @@
-from app.models.universitie import University
-from app.repositories.university_repository import UniversityRepository
+from app.models.university import Specialty, University
+from app.repositories.university_repository import SpecialtyRepository, UniversityRepository
 
 
 class UniversityService:
@@ -7,10 +7,36 @@ class UniversityService:
         self.repository = repository
 
     async def get_all_universities(self) -> list[University]:
-        return await self.repository.get_all_universities()
+        return await self.repository.get_all()
 
-    async def get_university_by_id(self, university_id) -> University | None:
-        return await self.repository.get_university_by_id(university_id)
+    async def get_university_by_id(self, university_id: int) -> University | None:
+        return await self.repository.get_by_id(university_id)
 
-    async def create_university(self, name) -> University:
-        return await self.repository.create_university(name)
+    async def create_university(self, name: str) -> University:
+        return await self.repository.create({"name": name})
+
+    async def update_university(self, university: University, name: str) -> University:
+        return await self.repository.update(university, {"name": name})
+
+    async def delete_university(self, university: University) -> None:
+        await self.repository.delete(university)
+
+
+class SpecialtyService:
+    def __init__(self, repository: SpecialtyRepository):
+        self.repository = repository
+
+    async def get_all_specialties(self) -> list[Specialty]:
+        return await self.repository.get_all()
+
+    async def get_specialty_by_id(self, specialty_id: int) -> Specialty | None:
+        return await self.repository.get_by_id(specialty_id)
+
+    async def create_specialty(self, data: dict) -> Specialty:
+        return await self.repository.create(data)
+
+    async def update_specialty(self, specialty: Specialty, data: dict) -> Specialty:
+        return await self.repository.update(specialty, data)
+
+    async def delete_specialty(self, specialty: Specialty) -> None:
+        await self.repository.delete(specialty)
