52004f0cd140f20cb002b9a8b5a316f21c0f3834
pecheritsa <pecheritsa.roman@gmail.com>
2025-06-11 21:11:13 +0300

    includes routers semester_router, subject_router, work_router

--- app/main.py
+++ app/main.py
@@ -2,17 +2,17 @@ from fastapi import FastAPI
 
 from app.core.config import get_settings
 from app.middleware import log_requests_middleware
-from app.routes.university.semester_routes import router as semester_router
-from app.routes.university.specialty_routes import router as specialty_router
-from app.routes.university.university_routes import router as university_router
+from app.routers.university import semester_router, speciality_router, subject_router, university_router, work_router
 
 settings = get_settings()
 
 app = FastAPI(debug=settings.DEBUG)
 app.middleware("http")(log_requests_middleware)
 app.include_router(university_router)
-app.include_router(specialty_router)
+app.include_router(speciality_router)
 app.include_router(semester_router)
+app.include_router(subject_router)
+app.include_router(work_router)
 
 
 @app.get("/")
