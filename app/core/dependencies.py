cb54ca542f9f847c97f5e0dbcfb27fe8a3c7d865
pecheritsa <pecheritsa.roman@gmail.com>
2025-06-11 13:52:59 +0300

    add specialty depends

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -2,8 +2,8 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db import get_db
-from app.repositories.university_repository import UniversityRepository
-from app.services.university_service import UniversityService
+from app.repositories.university_repository import SpecialtyRepository, UniversityRepository
+from app.services.university_service import SpecialtyService, UniversityService
 
 
 # ==== University Dependencies ====
@@ -13,3 +13,12 @@ def get_university_repository(session: AsyncSession = Depends(get_db)) -> Univer
 
 def get_university_service(repo: UniversityRepository = Depends(get_university_repository)) -> UniversityService:
     return UniversityService(repo)
+
+
+# ==== Specialty Dependencies ====
+def get_specialty_repository(session: AsyncSession = Depends(get_db)) -> SpecialtyRepository:
+    return SpecialtyRepository(session)
+
+
+def get_specialty_service(repo: SpecialtyRepository = Depends(get_specialty_repository)) -> SpecialtyService:
+    return SpecialtyService(repo)
