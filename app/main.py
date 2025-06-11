ceef71d566ba79d8e4ef42ca8aa0cd68dfffc4cd
pecheritsa <pecheritsa.roman@gmail.com>
2025-06-11 13:54:22 +0300

    add specialty_router

--- app/main.py
+++ app/main.py
@@ -2,13 +2,15 @@ from fastapi import FastAPI
 
 from app.core.config import get_settings
 from app.middleware import log_requests_middleware
-from app.routes.university_routes import router as university_router
+from app.routes.university.specialty_routes import router as specialty_router
+from app.routes.university.university_routes import router as university_router
 
 settings = get_settings()
 
 app = FastAPI(debug=settings.DEBUG)
 app.middleware("http")(log_requests_middleware)
 app.include_router(university_router)
+app.include_router(specialty_router)
 
 
 @app.get("/")
