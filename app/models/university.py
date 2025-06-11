1d1ed78ff1610acbd3e7d365a115a26a74929892
pecheritsa <pecheritsa.roman@gmail.com>
2025-06-11 21:06:05 +0300

    add cascade on_delete, rename fields

--- app/models/university.py
+++ app/models/university.py
@@ -12,32 +12,32 @@ class University(Base):
 
     name: Mapped[str] = mapped_column(String(100), unique=True)
 
-    specialties: Mapped[list["Specialty"]] = relationship(back_populates="university", cascade="all, delete-orphan")
+    specialties: Mapped[list["Speciality"]] = relationship(back_populates="university", cascade="all, delete-orphan")
 
 
-class Specialty(Base):
+class Speciality(Base):
     __tablename__ = "specialties"
 
     university_id: Mapped[int] = mapped_column(ForeignKey("universities.id", ondelete="CASCADE"))
     name: Mapped[str] = mapped_column(String(100))
-    total_cost: Mapped[int]
+    cost: Mapped[int]
     deadline: Mapped[date]
     semesters_count: Mapped[int]
     education_form_type: Mapped[EducationFormType]
 
     university: Mapped["University"] = relationship(back_populates="specialties")
-    semesters: Mapped[list["Semester"]] = relationship(back_populates="specialty", cascade="all, delete-orphan")
+    semesters: Mapped[list["Semester"]] = relationship(back_populates="speciality", cascade="all, delete-orphan")
 
 
 class Semester(Base):
     __tablename__ = "semesters"
 
-    specialty_id: Mapped[int] = mapped_column(ForeignKey("specialties.id", ondelete="CASCADE"))
+    speciality_id: Mapped[int] = mapped_column(ForeignKey("specialties.id", ondelete="CASCADE"))
     number: Mapped[int]
     cost: Mapped[int]
     deadline: Mapped[date]
 
-    specialty: Mapped["Specialty"] = relationship(back_populates="semesters")
+    speciality: Mapped["Speciality"] = relationship(back_populates="semesters")
     subjects: Mapped[list["Subject"]] = relationship(back_populates="semester", cascade="all, delete-orphan")
 
 
